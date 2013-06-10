82669c76a65d30b4ccda0da5febd6b34f2cc06ba
Tychio <code@tychio.net>
2013-06-10 10:53:08 +0800

    refactoring function content for unit test

--- unit/dialog_spec.js
+++ unit/dialog_spec.js
@@ -74,64 +74,21 @@ describe('Dialog', function () {
             expect(_val).toEqual(_testTitle);
         });
     });
-    xdescribe('Testing API function - content', function () {
-        it('exsit content function', function () {
-            expect(d.content).not.toBeUndefined();
+    describe('testing Content', function () {
+        it('function is avaliable', function () {
+            expect(dialog.content).toBeDefined();
         });
         it('getting the content of jQuery object', function () {
-            expect(d.content() instanceof jQuery).toBeTruthy();
+            expect(dialog.content() instanceof jQuery).toBeTruthy();
         });
         it('push a bar into the content and setting style', function () {
-            var _testId = 'jasmine_test_content';
-            var _testNo = getRandom(5) - 0;
-            d.content($('<div/>').attr('id', _testId), {
-                'zoom':  _testNo
+            var _testId = 'jasmine';
+            var _testStyle = '983px';
+            dialog.content($('<div/>').attr('id', _testId), {
+                'height':  _testStyle
             });
             expect($('#' + _testId).length).toBeGreaterThan(0);
-            expect($('#' + _testId).parent().css('zoom') - 0).toEqual(_testNo);
-        });
-        describe('Testing API function - input(refer content)', function () {
-            it('setting the input attr', function () {
-                var _testAttr = getRandom(5);
-                d.input({
-                    'data-attr': _testAttr,
-                    'inputtype': 'password'
-                });
-                var $input = d.content().children().last().find('input');
-                expect($input.attr('data-attr')).toEqual(_testAttr);
-                expect($input.attr('type')).toEqual('password');
-            });
-            it('create textarea', function () {
-                d.input({}, '', true);
-                var $bar = d.content().children().last();
-                expect($bar.find('textarea').length).toBeGreaterThan(0);
-                expect($bar.children().last()[0].tagName.toLocaleLowerCase()).toEqual('textarea');
-            });
-            it('add event for enter', function () {
-                var flag, enter, key;
-                runs(function () {
-                    flag = false;
-                    enter = 0;
-                    key = -1;
-                    d.input({}, '', false, function (p_e) {
-                        flag = true;
-                        key = p_e.which || p_e.keyCode;
-                    });
-                    setTimeout(function () {
-                        var _e = $.Event('keyup');
-                        _e.which = 13;
-                        d.content().children().last().find('input').trigger(_e);
-                    }, 200);
-                });
-                waitsFor(function () {
-                    enter++;
-                    return flag;
-                }, 'trigger the input enter event', 1000);
-                runs(function () {
-                    expect(enter).toBeGreaterThan(0);
-                    expect(key).toEqual(13);
-                });
-            });
+            expect($('#' + _testId).parent().css('height')).toEqual(_testStyle);
         });
     });
     xdescribe('Testing API function - tip', function () {
@@ -161,6 +118,49 @@ describe('Dialog', function () {
         it('trigger click button', function () {
             $('#' + _testSet.id).click();
             expect(_testClick).toBeTruthy();
+        });
+    });
+    xdescribe('Testing API function - input(refer content)', function () {
+        it('setting the input attr', function () {
+            var _testAttr = getRandom(5);
+            d.input({
+                'data-attr': _testAttr,
+                'inputtype': 'password'
+            });
+            var $input = d.content().children().last().find('input');
+            expect($input.attr('data-attr')).toEqual(_testAttr);
+            expect($input.attr('type')).toEqual('password');
+        });
+        it('create textarea', function () {
+            d.input({}, '', true);
+            var $bar = d.content().children().last();
+            expect($bar.find('textarea').length).toBeGreaterThan(0);
+            expect($bar.children().last()[0].tagName.toLocaleLowerCase()).toEqual('textarea');
+        });
+        it('add event for enter', function () {
+            var flag, enter, key;
+            runs(function () {
+                flag = false;
+                enter = 0;
+                key = -1;
+                d.input({}, '', false, function (p_e) {
+                    flag = true;
+                    key = p_e.which || p_e.keyCode;
+                });
+                setTimeout(function () {
+                    var _e = $.Event('keyup');
+                    _e.which = 13;
+                    d.content().children().last().find('input').trigger(_e);
+                }, 200);
+            });
+            waitsFor(function () {
+                enter++;
+                return flag;
+            }, 'trigger the input enter event', 1000);
+            runs(function () {
+                expect(enter).toBeGreaterThan(0);
+                expect(key).toEqual(13);
+            });
         });
     });
     xdescribe('Testing API function - clear', function () {
